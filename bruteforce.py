60f42810767865f51ab0d7063f1d95942570ed64
Ryan Oliveira <ryanfgolivera189@gmail.com>
2026-09-01 12:34:54 -0300

    Create bruteforce.py

---
+++ bruteforce.py
@@ -0,0 +1,32 @@
+from string import digits
+from random import randint
+ 
+senha = str(randint(00000,99999)).zfill(5)
+
+tentativa = ""
+
+achou = False
+
+for i in digits:
+    for j in digits:
+        for k in digits:
+            for l in digits:
+                for m in digits:
+                    print(i,j,k,l,m)
+
+                    tentativa = i+j+k+l+m
+
+                    if tentativa == senha:  
+                        achou = True
+                    if achou:
+                        break
+                if achou:
+                    break
+            if achou: 
+                break
+        if achou:
+            break
+    if achou:
+        break
+
+print("A senha é", senha)
